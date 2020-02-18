3850c4e08051c47cdff6378187bc55e13befaaa3
nsmontar <nsmontar@gmail.com>
2020-02-18 16:14:11 +0100

    Add project files.

---
+++ Ch10CardClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch10CardLib;
+using static System.Console;
+
+namespace Ch10CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Write(myDeck.GetCard(i).ToString());
+                if (i != 51) Write(", ");
+                else WriteLine();
+
+            }
+            ReadKey();
+        }
+    }
+}
