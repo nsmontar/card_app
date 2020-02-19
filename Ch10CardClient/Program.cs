c2f98942be3fc71cedc226408c1adbc9cd3224b6
nsmontar <nsmontar@gmail.com>
2020-02-19 10:47:48 +0100

    Added flush detection functionality

--- Ch10CardClient/Program.cs
+++ Ch10CardClient/Program.cs
@@ -21,6 +21,33 @@ namespace Ch10CardClient
                 else WriteLine();
 
             }
+            bool flushFlag = false;
+            Random rndGen = new Random();
+            int[] deckIndexArray = new int[5];
+            Card[] tempCardArray = new Card[5];
+            Suit[] drawedSuits = new Suit[5];
+            for (int i = 1; i <= 50; i++)
+            {
+                do
+                {
+                    for (int j = 0; j < deckIndexArray.Length; j++)
+                        deckIndexArray[j] = rndGen.Next(51);
+                } while (deckIndexArray.Distinct().Count() < deckIndexArray.Length);
+                for (int j = 0; j < deckIndexArray.Length; j++)
+                {
+                    tempCardArray[j] = myDeck.GetCard(deckIndexArray[j]);
+                    drawedSuits[j] = tempCardArray[j].suit;
+                }
+                if (drawedSuits.Distinct().Count() == 1)
+                {
+                    WriteLine($"\nFlush at {i}. draw!");
+                    Array.ForEach(tempCardArray, WriteLine);
+                    flushFlag = true;
+                    break;
+                }
+            }
+            if (!flushFlag)
+                WriteLine("\nNo Flush!");
             ReadKey();
         }
     }
