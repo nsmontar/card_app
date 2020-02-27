7e970f76fd70dc774692e2268fa5f777f93bfdca
nsmontar <nsmontar@gmail.com>
2020-02-27 16:36:47 +0100

    Modified Cards class to use List<Card> generic collection.

--- Ch10CardLib/Cards.cs
+++ Ch10CardLib/Cards.cs
@@ -7,17 +7,8 @@ using System.Collections;
 
 namespace Ch10CardLib
 {
-    class Cards : CollectionBase, ICloneable
+    class Cards : List<Card>, ICloneable
     {
-        public void Add(Card newCard)
-            => List.Add(newCard);
-        public void Remove(Card newCard)
-            => List.Remove(newCard);
-        public Card this[int cardIndex]
-        {
-            get => (Card)List[cardIndex];
-            set => List[cardIndex] = value;
-        }
         ///<summary>
         /// Untility method for copying card instances into another Cards
         /// instance=used in Deck.Shuffle(). This implementation assumes that
@@ -28,17 +19,11 @@ namespace Ch10CardLib
             for (int index = 0; index < this.Count; index++)
                 targetCards[index] = this[index];
         }
-        ///<summary>
-        /// Check to see if the Cards collection contains a particular card.
-        /// This calls the Contains() method of the ArrayList for the colletion,
-        /// which you access through the InnerList property.
-        /// </summary>
-        public bool Contains(Card myCard)
-            => InnerList.Contains(myCard);
+
         public object Clone()
         {
             Cards copiedCards = new Cards();
-            foreach (Card sourceCard in List)
+            foreach (Card sourceCard in this)
                 copiedCards.Add((Card)sourceCard.Clone());
             return copiedCards;
         }
